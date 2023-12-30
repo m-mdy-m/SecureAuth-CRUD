2b42e1c84b3bb45c15c535441a7f3a5835161f84
mahdi <mahdimamashli1383@gmail.com>
2023-12-30 22:33:35 +0330

    Hash password before saving to database.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-
+const bcryptjs  = require('bcryptjs')
 exports.getSignup = (req, res) => {
   res.render("auth/signup", {
     title: "signup",
@@ -10,12 +10,13 @@ exports.postSignUp = async (req, res) => {
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
+  const hashedPass = await bcryptjs.hash(password , 8)
   let user = await User.findOne({email})
   if(user){
     console.log('is user')
     return res.redirect('/')
   }
-  user = await User.create({ username, email, password });
+  user = await User.create({ username, email, password: hashedPass });
   await user.save()
    res.redirect('/login')
 };
@@ -23,6 +24,6 @@ exports.getLogin = async (req,res)=>{
     res.render('auth/login', {
         title : 'login',
         path : req.path,
-        
+
     })
 }
