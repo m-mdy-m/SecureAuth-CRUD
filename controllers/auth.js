f17db25bfa3579567cdf8128662dccfed4e94f62
mahdi <mahdimamashli1383@gmail.com>
2023-12-30 22:07:41 +0330

    create route and controllers signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,16 @@
-const User = require('../models/User')
+const User = require("../models/User");
 
-exports.getSignup  = (req,res)=>{
-    res.render('auth/signup', {
-        title : "signup",
-        path : req.path
-    })
-}
+exports.getSignup = (req, res) => {
+  res.render("auth/signup", {
+    title: "signup",
+    path: req.path,
+  });
+};
+exports.postSignUp = async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = await User.create({ username, email, password });
+  await user.save()
+  res.redirect('/')
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const authC = require('../controllers/auth')
 
 router.get('/signup', authC.getSignup)
-
+router.post('/signup', authC.postSignUp)
 module.exports = router
