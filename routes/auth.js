bca6ea353af1b0e66efee679af68c88316e021f6
mahdi <mahdimamashli1383@gmail.com>
2023-12-30 22:17:07 +0330

    Add login route and create template login

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,6 @@ const authC = require('../controllers/auth')
 
 router.get('/signup', authC.getSignup)
 router.post('/signup', authC.postSignUp)
+
+router.get('/login', authC.getLogin)
 module.exports = router
